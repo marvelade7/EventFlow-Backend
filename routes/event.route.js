32ab982f201ad384e6c6ce4937e6a98df996c7a8
Adewuyi Marvellous <marveladeadewuyi@gmail.com>
2026-04-29 12:33:35 +0100

    Add multer middleware for banner image upload in event creation route

--- routes/event.route.js
+++ routes/event.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { createEvent } = require('../controllers/event.controller');
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router()
 
-router.post('/create-event', authMiddleware, createEvent)
+router.post('/create-event', authMiddleware, upload.single("banner"), createEvent)
 
 module.exports = router
